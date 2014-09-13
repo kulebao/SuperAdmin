ef0888a3043d3db4ec4c6bdfb795bdbb8e4c38e9
Suo Qin <kpsesuo@gmail.com>
2014-09-13 23:38:41 +0800

    init repo

---
+++ SuperAdmin/SuperAdmin-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  BridgeHeader.h
+//  SuperAdmin
+//
+//  Created by kpse on 9/13/14.
+//  Copyright (c) 2014 cocobabys. All rights reserved.
+//
+
+#ifndef SuperAdmin_BridgeHeader_h
+#define SuperAdmin_BridgeHeader_h
+
+#import <AFNetworking/AFNetworking.h>
+
+#endif
