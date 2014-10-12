efcd7c6f76dd126064bec0b7c1662c9f4dcd9199
Suo Qin <kpsesuo@gmail.com>
2014-10-13 00:12:39 +0800

    add more fields value to edit school controller

--- SuperAdmin/EditSchoolController.swift
+++ SuperAdmin/EditSchoolController.swift
@@ -10,9 +10,27 @@ import Foundation
 
 class EditSchoolController: UIViewController {
     var school : School!
-
+    
     @IBOutlet weak var schoolId: UILabel!
+    @IBOutlet weak var schoolName: UITextField!
+    @IBOutlet weak var shortName: UITextField!
+    @IBOutlet weak var contact: UITextField!
+    @IBOutlet weak var token: UITextField!
+    @IBOutlet weak var loginName: UILabel!
+    @IBOutlet weak var expire: UILabel!
+    @IBOutlet weak var memberLimitation: UILabel!
+    @IBOutlet weak var address: UILabel!
+    
     override func viewDidLoad() {
         self.schoolId!.text = "\(self.school.id)"
+        self.schoolName!.text = self.school.name
+        self.shortName!.text = self.school.name
+        self.contact!.text = self.school.principal.phone
+        self.token!.text = "token"
+        self.loginName!.text = self.school.principal.loginName
+        self.expire!.text = "1999-01-01"
+        self.memberLimitation!.text = "99"
+        self.address!.text = "Somewhere in China"
+        
     }
 }
